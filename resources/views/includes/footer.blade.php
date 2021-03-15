6534bf200a0076e080c75f0f94e652b3b82e5bee
VersaHasnaAfifah <yayangversa20@gmail.com>
2021-03-15 09:41:06 +0700

    Tugas Versa

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,14 @@
+<script src="{{ asset('assets/js/bootstrap.bundle.min.js')}}"></script>
+
+<footer class=" text-white">
+      <div class="container">
+        <div class="row">
+          <div class="col text-center">
+            <p>Copyright Versa Hasnan Afifah</p>
+            <p>5520119014</p>
+            <p>INFORMATIKA A 2019</p>
+            
+          </div>
+        </div>
+      </div>
+    </footer>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+@section('title','halaman about')
+@section('content')
+<x-modal warna="danger" link="/contact" modal="About">
+    SELANJUTNYA
+</x-modal>
+<div class="container" style="height:500px; background-image: url(assets/gambar/section4.png);">
+          <div class="row mb-4">
+            <div class="col text-left ml-4">
+              <h1>ABOUT KATAILHAM</h1>
+            </div>
+          </div>
+          <div class="row content-left ml-5">
+            <div class="col-md-7">
+              <p>       
+              Dalam organisasi Paskibraka, ada dua lambang, yaitu lambang Paskibraka/Paskibra yaitu bergambarkan dua pemuda/pemudi paskibraka menengok kekanan dengan seragam PDU adalah lambang aktif anggota paskibra/paskibraka yang sedang bertugas, dan ada lambang kedua yaitu lambang Purna Paskibraka Indonesia yang berlambangkan daun dan bunga teratai. Penjelasan lambangnya sebagai berikut:
+            tiga helai daun yang tumbuh ke atas: artinya paskibraka harus belajar, bekerja, dan berbakti
+            tiga helai daun yang tumbuh mendatar/samping: artinya seorang pakibra harus aktif, disiplin, dan bergembira
+            Artinya adalah bahwa setiap anggota paskibraka memiliki jiwa yang sangat mulia. dan mengapa Lambang Anggota Paskibraka dilambangkan dengan Bunga Teratai. Karena Bunga Teratai tumbuh di lumpur dan berkembang diatas air yang bermakna bahwa anggota Paskibraka adalah pemuda dan pemudi yang tumbuh dari (Orang Biasa) tanah air yang sedang bermekar/berkembang dan membangun
+              </p>
+            </div>
+          </div>
+        </div>
+@stop
